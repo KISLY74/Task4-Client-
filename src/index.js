7b93f54237360af983cdec24164c23ae01a68ad5
KISLY74 <kisly.nikita@mail.ru>
2022-08-01 18:11:26 +0300

    feat: set routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
+import Main from './pages/Main';
+import Authorization from './pages/Authorization';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <Routes>
+        <Route path="regin" element={<Authorization />} />
+        <Route path="login" element={<Authorization />} />
+        <Route path="users" element={<Main />} />
+        <Route path="*" element={<Navigate to="/users" />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
