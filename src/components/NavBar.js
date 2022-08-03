f58620a2532add79f679c1c97653ee750cee3292
KISLY74 <kisly.nikita@mail.ru>
2022-08-03 16:22:21 +0300

    feat: create NavBar, with data of the store mobx check user on the authorization and display button logout and authorization

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import { Button, Navbar } from "react-bootstrap"
+import Nav from "react-bootstrap/Nav"
+import { useContext } from "react"
+import { useNavigate } from "react-router-dom"
+import { observer } from "mobx-react-lite"
+import { Context } from "../index"
+
+const NavBar = observer(() => {
+  const history = useNavigate()
+  const userName = localStorage.getItem("userName")
+  const { user } = useContext(Context)
+  const logOut = () => {
+    user.setUser({})
+    user.setIsAuth(false)
+    localStorage.clear()
+  }
+  return (
+    <Navbar className="d-flex justify-content-between" style={{ paddingLeft: 15, paddingRight: 15 }
+    } bg="dark" variant="dark" >
+      < h6 style={{ color: 'white' }}> {userName ? userName : ""}</h6 >
+      {
+        user.isAuth ? <Nav className="ml-auto" style={{ color: 'white' }
+        } >
+          <Button variant={"outline-light"} onClick={() => logOut()} > Выйти</Button>
+        </Nav > :
+          <Nav style={{ color: 'white' }}>
+            <Button variant={"outline-light"} onClick={() => history("/login")} >Авторизация</Button>
+          </Nav>
+      }
+    </Navbar >
+  )
+})
+
+export default NavBar
