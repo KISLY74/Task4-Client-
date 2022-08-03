15fe7b2db7679d840bd42dd4a0885a6be11a0628
KISLY74 <kisly.nikita@mail.ru>
2022-08-03 16:27:50 +0300

    feat: wrappe up in Context.Provider App component for use store

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
-import Main from './pages/Main';
-import Authorization from './pages/Authorization';
+import UserStore from './store/UserStore';
+
+export const Context = createContext(null)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Context.Provider value={{
+      user: new UserStore()
+    }}>
       <App />
-      <Routes>
-        <Route path="regin" element={<Authorization />} />
-        <Route path="login" element={<Authorization />} />
-        <Route path="users" element={<Main />} />
-        <Route path="*" element={<Navigate to="/users" />} />
-      </Routes>
-    </BrowserRouter>
+    </Context.Provider>
   </React.StrictMode>
 );
