6b00f92201cbb7729d98bb5b4c3e79b317d7fa5c
KISLY74 <kisly.nikita@mail.ru>
2022-08-04 12:08:45 +0300

    feat: add variable isblock in the store

--- src/store/UserStore.js
+++ src/store/UserStore.js
@@ -4,6 +4,7 @@ export default class UserStore {
   constructor() {
     this._isAuth = false
     this._user = {}
+    this._isBlock = false
     makeAutoObservable(this)
   }
   setIsAuth(bool) {
@@ -12,10 +13,16 @@ export default class UserStore {
   setUser(user) {
     this._user = user
   }
+  setIsBlock(bool) {
+    this._isBlock = bool
+  }
   get isAuth() {
     return this._isAuth
   }
   get user() {
     return this._user
+  }
+  get isBlock() {
+    return this._isBlock
   }
 }
