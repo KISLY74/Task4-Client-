31e9fdcf5030a5a7bda952deda80bc2754aaecd5
KISLY74 <kisly.nikita@mail.ru>
2022-08-05 15:14:31 +0300

    fix: change none exist route to users

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,16 +1,22 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import Main from '../pages/Main';
 import Authorization from '../pages/Authorization';
+import { useContext } from 'react';
+import { Context } from '../index';
+import { observer } from 'mobx-react-lite';
 
-const AppRouter = () => {
+const AppRouter = observer(() => {
+  const user = useContext(Context)
+  const history = useNavigate()
+  if (user.isAuth === false) history("/login")
   return (
     <Routes>
       <Route path="regin" element={<Authorization />} />
       <Route path="login" element={<Authorization />} />
       <Route path="users" element={<Main />} />
-      <Route path="*" element={<Navigate to="/login" />} />
+      <Route path="*" element={<Navigate to="/users" />} />
     </Routes>
   )
-}
+})
 
 export default AppRouter
