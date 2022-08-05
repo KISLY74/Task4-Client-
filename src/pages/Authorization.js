0ce54d068c5f442c3fb895b7684ce5d3bc63d9d7
KISLY74 <kisly.nikita@mail.ru>
2022-08-05 19:12:37 +0300

    feat: add spiner for loading when user log in

--- src/pages/Authorization.js
+++ src/pages/Authorization.js
@@ -1,7 +1,7 @@
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { regin, login, changeDateLogin } from "../http/userApi";
 import { useContext, useState } from "react";
-import { Container, Card, Form, Button, Row } from "react-bootstrap"
+import { Container, Card, Form, Button, Row, Spinner } from "react-bootstrap"
 import { Context } from "../index";
 import { getOneUser } from "../http/userApi";
 
@@ -14,7 +14,9 @@ function Authorization() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const { user } = useContext(Context)
-  const click = async () => {
+  const [isLoading, setLoading] = useState(true)
+  const handleClickBtn = async () => {
+    setLoading(false)
     try {
       let res
       if (isLogin) {
@@ -23,7 +25,7 @@ function Authorization() {
           await changeDateLogin(email)
           data = await getOneUser(email).then(data => data)
           if (!data) alert("Пользователь удалён!")
-        }).catch((err) => alert(err.response.data.message))
+        }).catch((err) => alert(err.response.data.message)).finally(() => setLoading(true))
         if (data) {
           localStorage.setItem("userName", data.name)
           if (data.status === "Block") {
@@ -83,9 +85,19 @@ function Authorization() {
             </div> : <div>Есть аккаунт?
               <NavLink style={{ textDecoration: "none" }} to="/login"> Войдите!</NavLink>
             </div>}
-            <Button className="mt-3" variant={"outline-success"} onClick={() => click()}>{isLogin ? "Войти" : "Зарегистрироваться"}</Button>
+            <Button className="mt-3" variant={"outline-success"} onClick={() => handleClickBtn()}>{isLogin ? "Войти" : "Зарегистрироваться"}</Button>
           </Row>
         </Form>
+        {isLoading ? "" : <Button className="mt-3 d-flex justify-content-center" variant="dark">
+          <Spinner
+            as="span"
+            animation="grow"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          />
+          Loading...
+        </Button>}
       </Card>
     </Container>
   );
